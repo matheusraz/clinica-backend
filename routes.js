a608f15a23a67cb1940fdb5d02abe0843cb33500
Matheus Raz <mrol@cin.ufpe.br>
2019-08-26 02:36:15 -0300

    organizando

--- routes.js
+++ routes.js
@@ -1,18 +1,6 @@
 const router = require('express').Router();
 const fs = require('fs');
 
-responseMessage = (keys, values) => {
-    try {
-        let response = {};
-        for(let i = 0; i < keys.length; ++i) {
-            let key = keys[i];
-            response[key] = values[i];
-        }
-        return response;
-    } catch (err) {
-        console.log(err.stack);
-    }
-}
 
 router.get('/', (req, res) => {
     res.json(responseMessage(['status', 'msg'], ['1', 'Funfou!']));
@@ -173,6 +161,19 @@ const getDates = (startDate, stopDate) => {
         currentDate = currentDate.addDays(1);
     }
     return dateArray;
+}
+
+responseMessage = (keys, values) => {
+    try {
+        let response = {};
+        for(let i = 0; i < keys.length; ++i) {
+            let key = keys[i];
+            response[key] = values[i];
+        }
+        return response;
+    } catch (err) {
+        console.log(err.stack);
+    }
 }
 
 
